72726c68115f4b0a49f3a8ce1bac6ccbbdd56d7a
Edem Robin <robinedemamedzo@gmail.com>
2023-07-21 23:16:40 +0000

    I am currently having a cors issue

--- web_app/index.py
+++ web_app/index.py
@@ -1,34 +1,59 @@
-#!/usr/bin/python3
-
 import requests
-from cmd_app.setting import main
+import pyodide_http
+
+# endpoint = "https://api.macvendors.com/v1/lookup"
+
+# # #setting up all the elements
+# # company_name = Element("company")
+# mac_address = Element("mac")
+# show_mac = Element("show-mac")
 
 
-endpoint = "https://api.macvendors.com/v1/lookup"
+# company_name.write(name)
 
-#setting up all the elements
-company_name = Element("company")
 
-name = main()[0]
-bearer = main()[1]
-company_name.write(name)
 
-mac_address = input("Please enter mac address: ")
+
+# def run():
+bearer = ""
+
+#     # mac_address = input("Please enter mac address: ")
+#     headers = {
+#             "Authorization": bearer
+#     }
+
+#     pyodide_http.patch_all()
+#     response = requests.get(url=f"{endpoint}/{mac_address.value}", headers=headers)
+#     address_find = response.json()
+#     try:
+#         show_mac.write(f"DEVICE NAME: {address_find['data']['organization_name']}".upper())
+#         # return
+#     except:
+#         show_mac.write("Please enter a valid mac address")
+#         # return "Please enter a valid mac address"
+
+# def data():
+#     pass   
+
+
+# def main(*a,**ab):
+#     run()
+
+
+
+
+# Patch the Requests library so it works with Pyscript
+pyodide_http.patch_all()
+
+        # Make a request to the JSON Placeholder API
+# response = requests.get("https://api.macvendors.com/v1/lookup/000142")
+mac_address="000142"
 headers = {
-        "Authorization": bearer
+        
+        "Authorization": bearer,
+        "accept": "application/json",
+        "Access-Control-Allow-Origin":"*"
     }
+endpoint = "https://api.macvendors.com/v1/lookup"
 response = requests.get(url=f"{endpoint}/{mac_address}", headers=headers)
-address_find = response.json()
-
-def run():
-    try:
-        return f"DEVICE NAME: {address_find['data']['organization_name']}".upper()
-    except:
-        return "Please enter a valid mac address"
-
-def data():
-    pass   
-
-
-def main(*a,**ab):
-    pass
+print(response.json())
